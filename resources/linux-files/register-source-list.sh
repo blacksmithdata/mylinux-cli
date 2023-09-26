93215e0c7deaaab5c414ccb6135cf1d84585363e
Shah Z. S <shah@blacksmithdata.io>
2023-09-26 12:23:42 +0000

    refactor

--- resources/linux-files/register-source-list.sh
+++ resources/linux-files/register-source-list.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-if [ -d /etc/apt/sources.list.d ]; then
-  # echo "The directory /etc/apt/sources.list.d exists."
-  echo "deb [trusted=yes] https://raw.githubusercontent.com/blacksmithdata/mylinux-cli/master/ linux/mylinux-cli/alpine/" > /etc/apt/sources.list.d/mylinux-cli.list
-  echo "deb [trusted=yes] https://raw.githubusercontent.com/blacksmithdata/mylinux-cli/master/ linux/blacksmith-cli/alpine/" > /etc/apt/sources.list.d/blacksmith-cli.list
-else
-  #echo "The directory /etc/apt/sources.list.d does not exist."
+if [ ! -d /etc/apt/sources.list.d ]; then
   mkdir -p /etc/apt/sources.list.d
-  echo "deb [trusted=yes] https://raw.githubusercontent.com/blacksmithdata/mylinux-cli/master/ linux/mylinux-cli/alpine/" > /etc/apt/sources.list.d/mylinux-cli.list
-  echo "deb [trusted=yes] https://raw.githubusercontent.com/blacksmithdata/mylinux-cli/master/ linux/blacksmith-cli/alpine/" > /etc/apt/sources.list.d/blacksmith-cli.list
 fi
+
+echo "deb [trusted=yes] https://raw.githubusercontent.com/blacksmithdata/mylinux-cli/master/ deb/" > /etc/apt/sources.list.d/mylinux-cli.list
+echo "deb [trusted=yes] https://raw.githubusercontent.com/blacksmithdata/mylinux-cli/master/ debian-blacksmith-cli/" > /etc/apt/sources.list.d/blacksmith-cli.list
